a28597506292d3e38498cce1f84d250b985fb5f1
sebabelmar <si.belmar@gmail.com>
2014-04-09 21:23:21 -0700

    GPS 2.1 & Booggie board.

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -1,8 +1,9 @@
+
 # U2.W5: A Nested Array to Model a Boggle Board
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge Sebastian Belmar
 
-boggle_board = [["b", "r", "a", "e"],
+$boggle_board = [["b", "r", "a", "e"],
                 ["i", "o", "d", "t"],
                 ["e", "c", "l", "r"],
                 ["t", "a", "k", "e"]]
@@ -10,16 +11,26 @@ boggle_board = [["b", "r", "a", "e"],
 
 # Part 1: Access multiple elements of a nested array
 
-# Pseudocode
+# Pseudocode:
+# input row/column to access element in nested array
+# boggle_board[row][column], this is the way to access a nested array
 
 # Initial Solution
+def create_word(board, *coords)
+  coords.map { |coord| board[coord.first][coord.last]}.join("")
+end
 
+puts create_word($boggle_board, [2,1], [1,1], [1,2], [0,3])  #=> returns "code"  
+puts create_word($boggle_board, [0,1], [0,2], [1,2])  #=> creates what california slang word? 
+#This call create_word() returns the word, "rad"
 
 # Refactored Solution
-
+#We found this code to be clear, concise, and efficient, so we did not refactor it.
 
 # DRIVER TESTS GO BELOW THIS LINE
-
+p create_word($boggle_board, [0,0],[1,0],[3,2],[3,3]) == "bike"
+p create_word($boggle_board, [1,1]) == 'o'
+p $boggle_board != []
 
 # Reflection 
 
@@ -28,15 +39,36 @@ boggle_board = [["b", "r", "a", "e"],
 # Part 2: Write a method that takes a row number and returns all the elements in the row.  
 
 # Pseudocode
+# -- changed boggle_board to global variable because get_row() did not have access to it
+# -- define get_row to take a single integer parameter
+#  -return boggle_board row
 
 # Initial Solution
-
+#def get_row(row)
+#   $boggle_board[row]
+#end
 
 # Refactored Solution
+#We did not refactor this solution because it is easy to understand and it is only one line of code
+def get_row(row)
+    raise ArgumentError.new("You put in a negative row") if row < 0
+    $boggle_board[row]
+end
+    
+
 
 
 # DRIVER TESTS GO BELOW THIS LINE
+#tests to see if row is negative
+begin
+  get_row(-1)
+rescue ArgumentError => err
+  fail = err.message
+end
 
+p fail == "You put in a negative row"
+
+p get_row(2) == ["e", "c", "l", "r"]
 
 # Reflection 
 
@@ -46,15 +78,40 @@ boggle_board = [["b", "r", "a", "e"],
 
 # Part 3: Now write a method that takes a column number and returns all the elements in the column.
 
-# Pseudocode
+# Pseudocode:
+# Define method get_col that takes a single integer parameter. 
+# Return an array with all the elements of that column in order.
 
 # Initial Solution
+#def get_col(col)
+#    i = 0
+#    result = []
+#    while i < $boggle_board.length
+#    result << $boggle_board[i][col]
+#    i += 1
+#    end
+#    return result
+#end
 
 
 # Refactored Solution
-
+def get_col(col)
+   raise ArgumentError.new("You put in a negative column") if col < 0
+   result= []
+   $boggle_board.each_index {|x| result << $boggle_board[x][col]}
+   result
+end
 
 # DRIVER TESTS GO BELOW THIS LINE
+begin
+  get_col(-1)
+rescue ArgumentError => err
+  fail = err.message
+end
+p fail == "You put in a negative column"
+p get_col(2) == %w{a d l k}
+p get_col(3) == %w{e t r e} 
 
 
-# Reflection 
+
+# Reflection
--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,6 +1,6 @@
 # U2.W5: Bakery Challenge GPS
 
-# I worked on this challenge with: 
+# I worked on this challenge with: Zac Mitton
 
 
 
@@ -9,12 +9,30 @@
 
 # Our Refactored Solution
 
+def bakery_num(num_of_people, fav_food)
 
+  my_list = {"pie" => 8, "cake" => 6, "cookie" => 1}
+  raise ArgumentError.new("You can't make that food") if my_list.has_key?(fav_food) == false
 
+  order_qty = num_of_people / my_list[fav_food]
+  left = num_of_people % my_list[fav_food]
 
+  pie_qty = left/8
+  cake_qty = (left%8)/6
+  cookie_qty = (left%8)%6
+  
+  case fav_food
+  when "pie"
+    then pie_qty = order_qty
+  when "cake"
+    then cake_qty = order_qty
+  else
+    cookie_qty = order_qty
+  end
 
+  return "You need to make #{pie_qty} pie(s), #{cake_qty} cake(s), and #{cookie_qty} cookie(s)."
 
-
+end
 
 
 #DRIVER CODE-- DO NOT MODIFY ANYTHING BELOW THIS LINE (except in the section at the bottom)
@@ -34,6 +52,16 @@ p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(
 
 
 #  Reflection 
-
+# This was a fun, challenging but time consuming one. I think it's fair for a messy 
+# code like the one DBC gave us.
+# Is was easy to undertand what the method was doing as soon as we re arrange the format.
+# After understanding the logic of the method we start refactoring.
+# Zac is really good on reading logic and finding error. I was suprise how fast he was able
+# to find mistakes as soon as we modify the code and didn't work.
+# After a couple iterations we were able to right a pretty straight forward code.
+# We missed the bonus so I think we'll work on that later this week. Hopefully our code it
+# is easy to scale.
+# I remember how to use Case. I forgot the syntax of it and but Zac new exactly how to work
+# with it.
 
 
